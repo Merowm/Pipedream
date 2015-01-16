2603f7d9f272a2fadb9f7080adb4f2aeab8b6953
Flchsh <kat13smarjaanah@kamk.fi>
2015-01-16 14:00:28 +0200

    collectible prefabs

---
+++ Pipedream/Assets/Scripts/CollectPoints.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectPoints : MonoBehaviour {
+	GameObject score;
+	// Use this for initialization
+	void Start () {
+		score = GameObject.FindGameObjectWithTag ("Scoretext");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		score.GetComponent<CountScore>().AddScore(10);
+		Destroy (this.gameObject);
+	}
+}
---
+++ Pipedream/Assets/Scripts/CountScore.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CountScore : MonoBehaviour {
+
+	int points;
+	// Use this for initialization
+	void Start () {
+		guiText.text = points.ToString ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void AddScore(int newPoints)
+	{
+		points += newPoints;
+		guiText.text = points.ToString ();
+	}
+}
