d87a92f12baa3f52da0be2d11aea898675bbfdc8
Flchsh <kat13smarjaanah@kamk.fi>
2015-02-04 17:09:26 +0200

    game timer

--- Pipedream/Assets/Scripts/UI scripts/GoToSceneSprite.cs
+++ Pipedream/Assets/Scripts/UI scripts/GoToSceneSprite.cs
@@ -4,14 +4,17 @@ using System.Collections;
 public class GoToSceneSprite : MonoBehaviour
 {
     CountScore sc;
+    LevelTimer lt;
     public int IdOfSceneToGo;
 
     void Awake()
     {
         sc = FindObjectOfType<CountScore>();
+        lt = FindObjectOfType<LevelTimer>();
     }
     void OnMouseUpAsButton()
     {
+        lt.FinalTimeBonus();
         sc.FinalLevelScore();
         Application.LoadLevel("MenuScene");
     }
--- Pipedream/Assets/Scripts/UI scripts/LevelTimer.cs
+++ Pipedream/Assets/Scripts/UI scripts/LevelTimer.cs
@@ -4,21 +4,30 @@ using System.Collections;
 public class LevelTimer : MonoBehaviour {
 
     public float timeTimer;
+
+    GameObject statObj;
+    Statistics stats;
 	// Use this for initialization
 	void Awake ()
     {
         timeTimer = 0;
 	}
-	
+	void Start()
+    {
+        statObj = GameObject.FindGameObjectWithTag("statistics");
+        if (statObj != null)
+            stats = statObj.GetComponent<Statistics>();
+    }
 	// Update is called once per frame
 	void Update ()
     {
         timeTimer += Time.deltaTime;
+        guiText.text = ((int)timeTimer).ToString();
 	}
 
     public void FinalTimeBonus()
     {
         // Send final time to CountScore / Statistics?
-        
+       Debug.Log(stats.CountTimeBonus(timeTimer));
     }
 }
