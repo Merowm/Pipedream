1a8fc09eb17ae21af752df35a5c5308c69d0d3f6
Flchsh <kat13smarjaanah@kamk.fi>
2015-03-11 16:00:04 +0200

    some scribbling

---
+++ Pipedream/Assets/Scripts/UI scripts/testers/DistanceMeter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DistanceMeter : MonoBehaviour {
+
+    GameObject[] entrances;
+    GameObject[] exits;
+    float fullrun;
+    float donerun;
+	// Use this for initialization
+	void Start ()
+    {
+        entrances = GameObject.FindGameObjectsWithTag("EntranceGate");
+        exits = GameObject.FindGameObjectsWithTag("ExitGate");
+        foreach (GameObject e in entrances)
+        {
+            fullrun += e.transform.position.z;
+        }
+        foreach (GameObject e in exits)
+        {
+            fullrun += e.transform.position.z;
+        }
+        for (int i = 0; i < exits.Length; ++i)
+        {
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
