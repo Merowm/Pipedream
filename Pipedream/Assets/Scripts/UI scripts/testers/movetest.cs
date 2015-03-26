a7f5a774cbbee3a756035eac18c6f76718d8d025
Flchsh <kat13smarjaanah@kamk.fi>
2015-03-26 16:34:21 +0200

    hyperspace effects

--- Pipedream/Assets/Scripts/UI scripts/testers/movetest.cs
+++ Pipedream/Assets/Scripts/UI scripts/testers/movetest.cs
@@ -5,28 +5,28 @@ public class movetest : MonoBehaviour {
 
     public float zOffset;
     Vector3 offset;
-    bool passed;
+    //bool passed;
     
 	// Use this for initialization
 	void Start () {
         offset = new Vector3(0, 0, zOffset);
         
-        passed = false;
+        //passed = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         transform.position += offset * Time.deltaTime;
-        transform.Rotate(0, 0, 3 * Time.deltaTime);
-        if (!passed)
-        {
-            if (transform.position.z > 50)
-            {
-                Debug.Log("one more");
-                passed = true;
-                Destroy(this.gameObject);
-            }
-        }
+        //transform.Rotate(0, 0, 3 * Time.deltaTime);
+        //if (!passed)
+        //{
+        //    if (transform.position.z > 50)
+        //    {
+        //        Debug.Log("one more");
+        //        passed = true;
+        //        Destroy(this.gameObject);
+        //    }
+        //}
         
 	}
 }
