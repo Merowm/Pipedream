e84aaaf4cb5751744a3407116347cfd7a0201f7e
RaynoldTanYX <133121w@mymail.nyp.edu.sg>
2015-03-27 10:04:58 +0200

    object pooling for parts in infinite level
    creating and destroying during runtime was causing lag whenever the
    player reached the end of the first tube

--- Pipedream/Assets/Scripts/InfiniteLevel.cs
+++ Pipedream/Assets/Scripts/InfiniteLevel.cs
@@ -5,19 +5,27 @@ using System.Collections.Generic;
 public class InfiniteLevel : MonoBehaviour {
 
     //LOGIC
+    //spawn all the different parts, deactivate, and add them to the object pool
     //2 parts active at any time
     //parts are randomized
     //when player reaches end of first part, 
-    //despawn first part,
+    //deactivate first part,
     //shift everything else one tube length back (back to origin)
-    //spawn new part behind second part
+    //activate new part behind second part
     //repeat
+    //1 2
+    //  2
+    //2
+    //2 3
 
     //list of parts available to spawn
     public List<GameObject> availablePartsList = new List<GameObject>();
 
     //list of active parts in the world (for easy management)
-    public List<GameObject> activePartsList = new List<GameObject>();
+    private List<GameObject> activePartsList = new List<GameObject>();
+
+    //object pool
+    private List<GameObject> poolList = new List<GameObject>();
 
     //length of each part
     public int LengthOfPart = 1000;
@@ -29,9 +37,18 @@ public class InfiniteLevel : MonoBehaviour {
     public Vector3 startingOffset;
 
     void Awake(){
+        //spawn objects for pooling
+        foreach (GameObject go in availablePartsList)
+        {
+            GameObject go1 = Instantiate(go);
+            go1.transform.FindChild("HyperPart_x10").gameObject.SetActive(true);
+            go1.SetActive(false);
+            poolList.Add(go1);
+        }
+
         //spawn initial 2 tubes 
-        SpawnPart(Random.Range(0, availablePartsList.Count), new Vector3(startingOffset.x, startingOffset.y, 0));
-        SpawnPart(Random.Range(0, availablePartsList.Count), new Vector3(0, 0, LengthOfPart));
+        SpawnPart(Random.Range(0, poolList.Count), new Vector3(startingOffset.x, startingOffset.y, 0));
+        SpawnPart(Random.Range(0, poolList.Count), new Vector3(startingOffset.x, startingOffset.y, LengthOfPart));
     }
 	
 	// Update is called once per frame
@@ -45,28 +62,33 @@ public class InfiniteLevel : MonoBehaviour {
 
     //spawns specified part at specified position
     private void SpawnPart(int indexToSpawn, Vector3 position){
-        //instantiate specified part at specified position
-        GameObject newPart = Instantiate(availablePartsList [indexToSpawn], position, Quaternion.identity) as GameObject;
-        //add new part to list for easy management
-        activePartsList.Add(newPart);
+        //activate specified part at specified position
+        GameObject go = poolList [indexToSpawn];
+        go.SetActive(true);
+        go.transform.position = position;
+        //remove part from pool
+        poolList.Remove(go);
+        //add new part to active list for easy management
+        activePartsList.Add(go);
 
-        newPart.transform.FindChild("HyperPart_x10").gameObject.SetActive(true);
     }
 
     //when the player reaches the end of the first tube
     private void OnFirstPartEnd(){
 
-        //destroy first tube
-        Destroy(activePartsList [0]);
+        //deactivate first tube
+        GameObject go = activePartsList [0];
+        go.SetActive(false);
         //remove from list
-        activePartsList.RemoveAt(0);
-
+        activePartsList.Remove(go);
+        //add back to pool
+        poolList.Add(go);
         //shift second tube and player back to origin
         activePartsList [0].transform.position -= new Vector3(0, 0, LengthOfPart);
         player.transform.position -= new Vector3(0, 0, LengthOfPart);
 
         //spawn new tube
-        SpawnPart(Random.Range(0, availablePartsList.Count), new Vector3(startingOffset.x, startingOffset.y, LengthOfPart));
+        SpawnPart(Random.Range(0, poolList.Count), new Vector3(startingOffset.x, startingOffset.y, LengthOfPart));
     }
 
 }
