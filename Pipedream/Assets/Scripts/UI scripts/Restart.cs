2ec0f815b97afe83ede6c9467fc49e332089c53b
RaynoldTanYX <133121w@mymail.nyp.edu.sg>
2015-04-09 16:03:49 +0300

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Pipedream/Assets/Scripts/UI scripts/Restart.cs

--- Pipedream/Assets/Scripts/UI scripts/Restart.cs
+++ Pipedream/Assets/Scripts/UI scripts/Restart.cs
@@ -15,7 +15,7 @@ public class Restart : MonoBehaviour
         st.ResetScore();
         int level = st.GetCurrentLevel();
         string levelname = st.GetLevelNameAsString(level);
-        GameObject.FindWithTag("statistics").GetComponent<LoadScreen>().showLoader(levelname);
+        GameObject.FindWithTag("statistics").GetComponent<LoadScreen>().showLoader(levelname,false);
         Time.timeScale = 1.0f;
         Application.LoadLevel(levelname);
     }
