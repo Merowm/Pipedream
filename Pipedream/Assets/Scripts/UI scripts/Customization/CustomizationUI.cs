d4448fb72bd15970e026a8dcb8606fa6b9d319b1
Hulavanne <pyry.liljamo@dnainternet.net>
2015-04-15 12:40:48 +0300

    Customization update

---
+++ Pipedream/Assets/Scripts/UI scripts/Customization/CustomizationUI.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CustomizationUI : MonoBehaviour
+{
+    public List<GameObject> objectsUI;
+    public GameObject pauseScreen;
+
+	void Awake ()
+    {
+        pauseScreen = transform.FindChild("pauseScreen").gameObject;
+
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject != pauseScreen)
+            {
+                objectsUI.Add(child.gameObject);
+            }
+        }
+	}
+
+	void Update ()
+    {
+	    if (pauseScreen.activeSelf)
+        {
+            for (int i = 0; i < objectsUI.Count; i++)
+            {
+                objectsUI[i].SetActive(false);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < objectsUI.Count; i++)
+            {
+                objectsUI[i].SetActive(true);
+            }
+        }
+	}
+}
