8c8960af00215bc4afa5733dcb8d6cf33e575059
Flchsh <kat13smarjaanah@kamk.fi>
2015-04-16 16:18:47 +0300

    new floating score points
    and unrelated cookie clicker (plus filing, again)

--- Pipedream/Assets/Scripts/UI scripts/FloatPointUI.cs
+++ Pipedream/Assets/Scripts/UI scripts/FloatPointUI.cs
@@ -4,19 +4,39 @@ using System.Collections;
 
 public class FloatPointUI : MonoBehaviour 
 {
-    public Canvas floatingPoints;
+    //public Canvas floatingPoints;
+    public GameObject pointsImage;
     private Canvas points;
+    private GameObject pointImg;
     Vector3 pointPosition;
-    public Vector3 offset;
+    //public Vector3 offset;
+
+    int cookieclickcount = 0;
+    void Start()
+    {
+        pointPosition = new Vector3(0, 120, 0);
+    }
 
     public void GeneratePoints(int scoreAmount)
     {
         // TODO: change position so it doesn't obstruct pipe view!
-        pointPosition = Camera.main.transform.position;
-        points = Instantiate(floatingPoints, pointPosition, Quaternion.identity) as Canvas;
-        points.transform.SetParent(Camera.main.transform, false);
-        points.transform.localPosition = offset;
-        points.GetComponent<Text>().text = scoreAmount.ToString();
+        //pointPosition = Camera.main.transform.position;
+        //points = Instantiate(floatingPoints, pointPosition, Quaternion.identity) as Canvas;
+        //points.transform.SetParent(Camera.main.transform, false);
+        //points.transform.localPosition = offset;
+        pointImg = Instantiate(pointsImage, pointPosition, Quaternion.identity) as GameObject;
+        pointImg.transform.SetParent(this.transform);
+        pointImg.transform.localPosition = pointPosition;
+        pointImg.GetComponent<Text>().text = scoreAmount.ToString();
 
     }
+    public void GeneratePointImage(int scoreAmount)
+    {
+        cookieclickcount++;
+        pointImg = Instantiate(pointsImage, pointPosition, Quaternion.identity) as GameObject;
+        pointImg.transform.SetParent(this.transform);
+        pointImg.transform.localPosition = pointPosition;
+        pointImg.GetComponent<Text>().text = cookieclickcount.ToString();
+    }
+
 }
