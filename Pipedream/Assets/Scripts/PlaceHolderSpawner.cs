38cf9ff207e8be1b82ef7cc3cd88da4d2d78fce5
Hulavanne <pyry.liljamo@dnainternet.net>
2015-05-06 16:30:30 +0300

    Obstacle and collectible stuff

--- Pipedream/Assets/Scripts/PlaceHolderSpawner.cs
+++ Pipedream/Assets/Scripts/PlaceHolderSpawner.cs
@@ -2,88 +2,95 @@
 using System.Collections;
 using System.Collections.Generic;
 
-[ExecuteInEditMode]
+//[ExecuteInEditMode]
 public class PlaceHolderSpawner : MonoBehaviour
 {
     public GameObject prefab;
     public Vector3 scale = new Vector3(0,0,0);
     public bool movingAsteroid = false;
+    public bool negativeRotation = false;
     public float wallRotatingSpeed = 0.0f;
     public float lotusRotatingSpeed = 0.0f;
-
-    public List<Material> materials;
 
     private Transform placeHolderChild;
     private GameObject obj;
 
 	void Awake ()
     {
-        if (Application.isPlaying)
+        placeHolderChild = transform.GetChild(0).transform;
+
+        obj = (GameObject)Instantiate(prefab, transform.position, transform.rotation);
+        obj.transform.parent = transform.parent;
+
+        if (negativeRotation)
         {
-            placeHolderChild = transform.GetChild(0).transform;
-
-            obj = (GameObject)Instantiate(prefab, transform.position, transform.rotation);
-            obj.transform.parent = transform.parent;
-
-            if (prefab.transform.name == "lotus")
+            wallRotatingSpeed = -wallRotatingSpeed;
+            lotusRotatingSpeed = -lotusRotatingSpeed;
+            if (prefab.transform.name == "lotus" || prefab.transform.name == "RotatingWall")
             {
-                placeHolderChild.localPosition = new Vector3(0,-5.5f,0);
+                obj.transform.GetComponent<ObstacleRotation>().rotation *= -1.0f;
             }
-            else if (prefab.transform.name == "HyperAsteroid01")
-            {
-                placeHolderChild.localPosition = new Vector3(0,-5.0f,0);
-            }
-
-            if (prefab.transform.name != "RotatingWall")
-            {
-                obj.transform.GetChild(0).transform.position = placeHolderChild.position;
-            }
-
-            if (prefab.transform.name == "lotus" && lotusRotatingSpeed != 0)
-            {
-                //obj = (GameObject)Instantiate(prefab, transform.position, transform.rotation);
-                //obj.transform.parent = transform.parent;
-                //obj.transform.GetChild(0).transform.localPosition = new Vector3(0,-4,0);
-
-                obj.transform.GetComponent<ObstacleRotation>().rotation = lotusRotatingSpeed;
-                Debug.Log("...");
-            }
-            else
-            {
-                //obj = (GameObject)Instantiate(prefab, placeHolderChild.position, placeHolderChild.rotation);
-                //obj.transform.parent = transform.parent;
-            }
-
-            if (scale != new Vector3(0,0,0))
-            {
-                foreach (Transform child in obj.transform)
-                {
-                    child.transform.localScale = scale;
-                }
-            }
-
-            if (prefab.transform.name == "HyperAsteroid01")
-            {
-                if (movingAsteroid)
-                {
-                    obj.GetComponent<ObstacleUpDown>().enabled = true;
-                }
-            }
-            else if (prefab.transform.name == "RotatingWall")
-            {
-                if (wallRotatingSpeed != 0)
-                {
-                    obj.transform.GetComponent<ObstacleRotation>().rotation = wallRotatingSpeed;
-                }
-            }
-
-            Destroy(placeHolderChild.gameObject);
-            Destroy(gameObject);
         }
-	}
+
+        if (prefab.transform.name == "lotus")
+        {
+            placeHolderChild.localPosition = new Vector3(0,-5.5f,0);
+        }
+        else if (prefab.transform.name == "HyperAsteroid01")
+        {
+            placeHolderChild.localPosition = new Vector3(0,-5.0f,0);
+        }
+
+        if (prefab.transform.name != "RotatingWall")
+        {
+            obj.transform.GetChild(0).transform.position = placeHolderChild.position;
+        }
+
+        if (prefab.transform.name == "lotus" && lotusRotatingSpeed != 0)
+        {
+            //obj = (GameObject)Instantiate(prefab, transform.position, transform.rotation);
+            //obj.transform.parent = transform.parent;
+            //obj.transform.GetChild(0).transform.localPosition = new Vector3(0,-4,0);
+
+            obj.transform.GetComponent<ObstacleRotation>().rotation = lotusRotatingSpeed;
+            Debug.Log("...");
+        }
+        else
+        {
+            //obj = (GameObject)Instantiate(prefab, placeHolderChild.position, placeHolderChild.rotation);
+            //obj.transform.parent = transform.parent;
+        }
+
+        if (scale != new Vector3(0,0,0))
+        {
+            foreach (Transform child in obj.transform)
+            {
+                child.transform.localScale = scale;
+            }
+        }
+
+        if (prefab.transform.name == "HyperAsteroid01")
+        {
+            if (movingAsteroid)
+            {
+                obj.GetComponent<ObstacleUpDown>().enabled = true;
+            }
+        }
+        else if (prefab.transform.name == "RotatingWall")
+        {
+            if (wallRotatingSpeed != 0)
+            {
+                obj.transform.GetComponent<ObstacleRotation>().rotation = wallRotatingSpeed;
+            }
+        }
+
+        Destroy(placeHolderChild.gameObject);
+        Destroy(gameObject);
+    }
+}
 
     //Color changing of the place holder object happens here
-#if UNITY_EDITOR
+/*#if UNITY_EDITOR
     void Update ()
     {
         if(UnityEditor.EditorApplication.isPlayingOrWillChangePlaymode)
@@ -92,15 +99,18 @@ public class PlaceHolderSpawner : MonoBehaviour
         }
         else
         {
-            //
             if (prefab.transform.name == "HyperAsteroid01")
             {
-                if (movingAsteroid)
+                if (!movingAsteroid)
                 {
-                    //transform.GetComponentInChildren<MeshRenderer>().material;
+                    transform.GetComponentInChildren<MeshRenderer>().material = materials[0];
+                }
+                else
+                {
+                    transform.GetComponentInChildren<MeshRenderer>().material = materials[1];
                 }
             }
         }
     }
-#endif
-}
+#endif*/
+//}
