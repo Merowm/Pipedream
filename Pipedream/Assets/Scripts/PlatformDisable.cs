371fb75111ac2a2c2e2031dadbfebdc5a63bd112
RaynoldTanYX <133121w@mymail.nyp.edu.sg>
2015-05-27 15:23:07 +0300

    asd

---
+++ Pipedream/Assets/Scripts/PlatformDisable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformDisable : MonoBehaviour {
+
+    public RuntimePlatform[] platformWhitelist;
+
+    void Awake()
+    {
+        foreach (RuntimePlatform platform in platformWhitelist)
+        {
+            if (Application.platform == platform)
+            {
+                return;
+            }
+        }
+        Destroy(gameObject);
+    }
+
+}
